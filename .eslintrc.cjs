b39a7ad27da85115c8937be186cf579064700c04
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-10-03 17:25:25 +0530

    initial commit, basic setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: { browser: false, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:import/errors",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+    "prettier"
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["import"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": 1,
+    quotes: ["error", "double"],
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-use-before-define": "off",
+    "no-multiple-empty-lines": [2, { max: 3, maxEOF: 1 }]
+  }
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# RSSB Backend
+
+This is the backend of the RSSB Application. It uses latest technologies like ExpressJS and Typescript. The database used here is MongoDB.
