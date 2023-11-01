28f5fb29e6d99773cddeac65ac3fb7605a9b8a3d
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-11-01 23:55:06 +0530

    setup env-var, config, and install argon2

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,57 +1,24 @@
-import path from "path";
 import dotenv from "dotenv";
+import env from "env-var";
+import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-// Parsing the env file.
 dotenv.config({ path: path.resolve(__dirname, "../../.env.local") });
 
-// Interface to load env variables
-// Note these variables can possibly be undefined
-// as someone could skip these varibales or not setup a .env file at all
+export const NODE_ENV = env
+  .get("NODE_ENV")
+  .required()
+  .asString()
+  .match(/^(development|production)$/)?.[0];
 
-interface ENV {
-  NODE_ENV: string | undefined;
-  PORT: number | undefined;
-  // MONGO_URI: string | undefined;
+if (!NODE_ENV) {
+  throw new Error("NODE_ENV must be either 'development' or 'production'.");
 }
 
-interface Config {
-  NODE_ENV: string;
-  PORT: number;
-  // MONGO_URI: string;
-}
+export const PORT = env.get("PORT").default(5000).asPortNumber();
 
-// Loading process.env as ENV interface
-
-const getConfig = (): ENV => {
-  return {
-    NODE_ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT ? Number(process.env.PORT) : undefined
-    // MONGO_URI: process.env.MONGO_URI
-  };
-};
-
-// Throwing an Error if any field was undefined we don't
-// want our app to run if it can't connect to DB and ensure
-// that these fields are accessible. If all is good return
-// it as Config which just removes the undefined from our type
-// definition.
-
-const getSanitizedConfig = (config: ENV): Config => {
-  for (const [key, value] of Object.entries(config)) {
-    if (value === undefined) {
-      throw new Error(`Missing key ${key} in config.env`);
-    }
-  }
-  return config as Config;
-};
-
-const config = getConfig();
-
-const sanitizedConfig = getSanitizedConfig(config);
-
-export default sanitizedConfig;
+export const MONGO_URI = env.get("MONGO_URI").required().asString();
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
-import express, { Request, Response, Application } from "express";
-import sanitizedConfig from "./config/config.js";
+import express, { Application, Request, Response } from "express";
+import { NODE_ENV, PORT } from "./config/config.js";
 
 const app: Application = express();
 
-const { PORT, NODE_ENV } = sanitizedConfig;
-
-const port = PORT || 8000;
-
 app.get("/", (req: Request, res: Response) => {
+  req.body;
   res.send("Welcome to Express & TypeScript Server");
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(
-    `Server is running at http://localhost:${port} in ${NODE_ENV} mode.`
+    `Server is running at http://localhost:${PORT} in ${NODE_ENV} mode.`
   );
 });
