dd712cfc220d11eb55c44c6b5f99547b0ee2ecfc
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-11-07 04:41:57 +0530

    api-response

--- src/utils/api/res/api-response.ts
+++ src/utils/api/res/api-response.ts
@@ -12,9 +12,45 @@ class ApiResponse {
   }
 }
 
-export class ApiSuccessResponse extends ApiResponse {
+export class SuccessResponse extends ApiResponse {
   constructor(message = "success", data: any) {
     super(200, message, data);
+  }
+}
+
+export class BadRequestResponse extends ApiResponse {
+  constructor(message = "Bad Request", data: any) {
+    super(400, message, data);
+  }
+}
+
+export class UnauthorizedResponse extends ApiResponse {
+  constructor(message = "Unauthorized", data: any) {
+    super(401, message, data);
+  }
+}
+
+export class ForbiddenResponse extends ApiResponse {
+  constructor(message = "Forbidden", data: any) {
+    super(403, message, data);
+  }
+}
+
+export class NotFoundResponse extends ApiResponse {
+  constructor(message = "Not Found", data: any) {
+    super(404, message, data);
+  }
+}
+
+export class InternalServerErrorResponse extends ApiResponse {
+  constructor(message = "Internal Server Error", data: any) {
+    super(500, message, data);
+  }
+}
+
+export class NotImplementedResponse extends ApiResponse {
+  constructor(message = "Not Implemented", data: any) {
+    super(501, message, data);
   }
 }
 
