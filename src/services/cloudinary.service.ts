7b9fc55b6c8ab99c83049223add0fb9dcd729ba2
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-12-27 18:36:11 +0530

    Add comments in CloudinaryService

--- src/services/cloudinary.service.ts
+++ src/services/cloudinary.service.ts
@@ -15,11 +15,13 @@ class CloudinaryService {
   constructor(cloud_name: string, api_key: string, api_secret: string) {
     this.cloudinaryResponse = null;
 
+    // configure cloudinary
     cloudinary.config({ cloud_name, api_key, api_secret });
   }
 
   uploadFileToCloudinary = async (localFilePath: string) => {
     try {
+      // check if file path is missing
       if (!localFilePath || localFilePath.length < 1) {
         printErrorMessage(
           "💀⚠️   No File Path Found!!",
@@ -27,6 +29,8 @@ class CloudinaryService {
         );
         return null;
       }
+
+      // check if file exists on the local server
       const response = await cloudinary.uploader.upload(localFilePath, {
         resource_type: "auto",
       });
@@ -67,6 +71,7 @@ class CloudinaryService {
 
   deleteFileFromCloudinary = async (fileURL: string) => {
     try {
+      // check if file URL is missing
       if (!fileURL || fileURL.length < 1) {
         printErrorMessage(
           "💀⚠️   No File URL Found!!",
@@ -75,6 +80,7 @@ class CloudinaryService {
         return null;
       }
 
+      // delete file from cloudinary
       const response = await cloudinary.uploader.destroy(fileURL);
 
       wLogger.info(`✅   File is deleted from Cloudinary: ${response}`);
