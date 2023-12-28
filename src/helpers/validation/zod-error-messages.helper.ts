99dcf9a15845c07d56ed93b38eb1a65e8668573d
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-12-28 14:48:03 +0530

    Add minStringError helper function and use it for validation

--- src/helpers/validation/zod-error-messages.helper.ts
+++ src/helpers/validation/zod-error-messages.helper.ts
@@ -6,4 +6,8 @@ const largeStringError = (fieldName: string, max: number) => {
   return fieldName + " must not exceed " + max + " characters.";
 };
 
-export { largeStringError, requiredError };
+const minStringError = (fieldName: string, min: number) => {
+  return fieldName + " must be at least " + min + " characters.";
+};
+
+export { largeStringError, minStringError, requiredError };
