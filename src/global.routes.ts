893e236e6c8abfb816672fecd9cf039e178b6586
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-10-05 16:29:28 +0530

    feat(api): redirect to /api-docs at /

--- src/global.routes.ts
+++ src/global.routes.ts
@@ -3,6 +3,11 @@ import { Router } from "express";
 
 const apiRouter = Router();
 
+// redirect to '/api-docs' at '/'
+apiRouter.get("/", (req, res) => {
+  res.redirect("/api-docs");
+});
+
 apiRouter.use("/api/v1", apiV1Router);
 // ... add more routers versions here ...
 
