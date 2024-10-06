b41028bd9a3a4d7f65efbb2107a640d043509d24
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-10-06 20:25:26 +0530

    feat(health): setup routes

--- src/api/v1/controllers/app-health.controller.ts
+++ src/api/v1/controllers/app-health.controller.ts
@@ -5,7 +5,7 @@ import { asyncHandler } from "@/common/utils/async-handler.util";
 import { Request, Response } from "express";
 
 export class AppHealthController {
-  public testAppHealth = asyncHandler(async (req: Request, res: Response) => {
+  public testAllHealth = asyncHandler(async (req: Request, res: Response) => {
     const google = await checkupService.httpCheck(ct.checkup.http.url);
     const db = await checkupService.dbCheck();
     const disk = await checkupService.diskCheck();
@@ -23,6 +23,58 @@ export class AppHealthController {
       new ApiResponse(200, "App health check successful!", {
         time: new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }),
         results,
+      })
+    );
+  });
+
+  public testAppHealth = asyncHandler(async (req: Request, res: Response) => {
+    res.status(200).json(
+      new ApiResponse(200, "Ping-Pong! App health check successful!", {
+        time: new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }),
+      })
+    );
+  });
+
+  public testDbHealth = asyncHandler(async (req: Request, res: Response) => {
+    const db = await checkupService.dbCheck();
+    res.status(200).json(
+      new ApiResponse(200, "Database health check successful!", {
+        time: new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }),
+        results: { db },
+      })
+    );
+  });
+
+  public testDiskHealth = asyncHandler(async (req: Request, res: Response) => {
+    const disk = await checkupService.diskCheck();
+    res.status(200).json(
+      new ApiResponse(200, "Disk health check successful!", {
+        time: new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }),
+        results: { disk },
+      })
+    );
+  });
+
+  public testMemoryHealth = asyncHandler(
+    async (req: Request, res: Response) => {
+      const memory = await checkupService.memoryCheck();
+      res.status(200).json(
+        new ApiResponse(200, "Memory health check successful!", {
+          time: new Date().toLocaleString("en-US", {
+            timeZone: "Asia/Kolkata",
+          }),
+          results: { memory },
+        })
+      );
+    }
+  );
+
+  public testHttpHealth = asyncHandler(async (req: Request, res: Response) => {
+    const google = await checkupService.httpCheck(ct.checkup.http.url);
+    res.status(200).json(
+      new ApiResponse(200, "HTTP health check successful!", {
+        time: new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }),
+        results: { google },
       })
     );
   });
--- src/api/v1/router/routes/app-health.routes.ts
+++ src/api/v1/router/routes/app-health.routes.ts
@@ -27,6 +27,86 @@ class AppHealthRouter {
      *         description: Health check failed
      */
     this.router.get("/", this.controller.testAppHealth);
+
+    /**
+     * @openapi
+     * /health/db:
+     *   get:
+     *     tags:
+     *       - Health
+     *     summary: Check the health of the database
+     *     description: Check the health of the database
+     *     responses:
+     *       200:
+     *         description: Health check passed
+     *       503:
+     *         description: Health check failed
+     */
+    this.router.get("/db", this.controller.testDbHealth);
+
+    /**
+     * @openapi
+     * /health/disk:
+     *   get:
+     *     tags:
+     *       - Health
+     *     summary: Check the health of the disk
+     *     description: Check the health of the disk
+     *     responses:
+     *       200:
+     *         description: Health check passed
+     *       503:
+     *         description: Health check failed
+     */
+    this.router.get("/disk", this.controller.testDiskHealth);
+
+    /**
+     * @openapi
+     * /health/memory:
+     *   get:
+     *     tags:
+     *       - Health
+     *     summary: Check the health of the memory
+     *     description: Check the health of the memory
+     *     responses:
+     *       200:
+     *         description: Health check passed
+     *       503:
+     *         description: Health check failed
+     */
+    this.router.get("/memory", this.controller.testMemoryHealth);
+
+    /**
+     * @openapi
+     * /health/http:
+     *   get:
+     *     tags:
+     *       - Health
+     *     summary: Check the health of the http
+     *     description: Check the health of the http
+     *     responses:
+     *       200:
+     *         description: Health check passed
+     *       503:
+     *         description: Health check failed
+     */
+    this.router.get("/http", this.controller.testHttpHealth);
+
+    /**
+     * @openapi
+     * /health/all:
+     *   get:
+     *     tags:
+     *       - Health
+     *     summary: Check the health of the all
+     *     description: Check the health of the all
+     *     responses:
+     *       200:
+     *         description: Health check passed
+     *       503:
+     *         description: Health check failed
+     */
+    this.router.get("/all", this.controller.testAppHealth);
   }
 }
 
