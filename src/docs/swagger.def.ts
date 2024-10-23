47a503df29e8aa5b6697dfeb7eb3ae1eb65b36f0
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-10-23 22:07:11 +0530

    fix: api docs security bearer (jwt)

--- src/docs/swagger.def.ts
+++ src/docs/swagger.def.ts
@@ -4,7 +4,7 @@ import ct from "@/common/constants";
 export const swaggerDefinition: swaggerJSDoc.OAS3Definition = {
   openapi: "3.0.0",
   info: {
-    title: `${ct.appName} - API Documentation`,
+    title: `${ct.appName} - REST API Documentation`,
     version: ct.appVersion,
     description: ct.appDescription,
   },
@@ -19,7 +19,12 @@ export const swaggerDefinition: swaggerJSDoc.OAS3Definition = {
   },
   security: [
     {
-      bearerAuth: [],
+      bearerAuth: [
+        "read:current_user", // this enables the user to read their own data
+        "write:current_user", // this enables the user to write their own data
+        "read:admin", // this enables the admin to read all data
+        "write:admin", // this enables the admin to write all data
+      ],
     },
   ],
   servers: [
